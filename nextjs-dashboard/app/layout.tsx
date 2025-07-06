cb40b27ad99f88215a401fadc67977bc2425187b
kangaroo19 <1000jjj@naver.com>
2025-07-06 14:15:22 +0900

    Add new feature to process user input
    Introduced a function to handle and validate user input, improving the robustness of the input processing workflow.

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,13 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+// 빌드 시 글꼴 파일을 다운로드하여
+// 다른 정적 애셋과 함께 호스팅합니다.
+//  즉, 사용자가 애플리케이션을 방문할 때 성능에 영향을 줄 수 있는
+//  글꼴에 대한 추가 네트워크 요청이 발생하지 않습니다.
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
